3f9ae003f11f9dc10a2e1a561056684fda0a2084
VadimTaranovscky <43213486+VadimTaranovscky@users.noreply.github.com>
2021-01-11 21:18:10 +0400

    feat(dataBase): change course page

--- routes/courses.js
+++ routes/courses.js
@@ -4,29 +4,52 @@ const Course = require("../models/course");
 const router = Router();
 
 router.get("/", async (req, res) => {
-  const courses = await Course.find();
-  res.render("courses", {
-    title: "Список курсов",
-    isCourses: true,
-    courses,
-  });
+  try {
+    const courses = await Course.find();
+    res.render("courses", {
+      title: "Список курсов",
+      isCourses: true,
+      courses,
+    });
+  } catch (e) {
+    console.log(e);
+  }
 });
 
 router.get("/:id/edit", async (req, res) => {
-  if (!req.query.allow) {
-    res.redirect("/courses");
-  } else {
-    const course = await Course.getCourse(req.params.id);
-    res.render("courseEdit", {
-      title: `Редактировать курс по ${course.title}`,
-      course,
-    });
+  try {
+    if (!req.query.allow) {
+      res.redirect("/courses");
+    } else {
+      const course = await Course.findById(req.params.id);
+      res.render("courseEdit", {
+        title: `Редактировать курс по ${course.title}`,
+        course,
+      });
+    }
+  } catch (e) {
+    console.log(e);
   }
 });
 
 router.post("/edit", async (req, res) => {
-  await Course.update(req.body);
-  res.redirect("/courses");
+  try {
+    const { id } = req.body;
+    delete req.body.id;
+    await Course.findByIdAndUpdate(id, req.body);
+    res.redirect("/courses");
+  } catch (e) {
+    console.log(e);
+  }
+});
+
+router.post("/remove", async (req, res) => {
+  try {
+    await Course.deleteOne({ _id: req.body.id });
+    res.redirect("/courses");
+  } catch (e) {
+    console.log(e);
+  }
 });
 
 module.exports = router;
