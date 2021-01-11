16bd893c54d19ec8163ea867bd8de3053794fe4f
VadimTaranovscky <43213486+VadimTaranovscky@users.noreply.github.com>
2021-01-11 21:19:22 +0400

    fix(dataBase): add try catch to add course

--- routes/addCourse.js
+++ routes/addCourse.js
@@ -11,13 +11,17 @@ router.get("/", (req, res) => {
 });
 
 router.post("/", async (req, res) => {
-  const course = new Course({
-    title: req.body.title,
-    price: req.body.price,
-    img: req.body.img,
-  });
-  await course.save();
-  res.redirect("/courses");
+  try {
+    const course = new Course({
+      title: req.body.title,
+      price: req.body.price,
+      img: req.body.img,
+    });
+    await course.save();
+    res.redirect("/courses");
+  } catch (e) {
+    console.log(e);
+  }
 });
 
 module.exports = router;
