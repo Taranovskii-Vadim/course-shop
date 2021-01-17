5cb92dedf3bf0e20f7ae36cd4bfc2186be6c4ea8
VadimTaranovscky <43213486+VadimTaranovscky@users.noreply.github.com>
2021-01-17 14:40:54 +0400

    feat(dataBase): add express-validator

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,9 @@
 const { Router } = require("express");
 const crypt = require("bcryptjs");
+const { validationResult } = require("express-validator");
 
 const User = require("../models/user");
+const { registerValidators, loginValidators } = require("../utils/validators");
 
 const router = Router();
 
@@ -14,48 +16,45 @@ router.get("/login", async (req, res) => {
   });
 });
 
-router.post("/signIn", async (req, res) => {
+router.post("/signIn", loginValidators, async (req, res) => {
   try {
-    const { email, password } = req.body;
+    const { email } = req.body;
     const candidate = await User.findOne({ email });
-    if (candidate) {
-      const isSame = await crypt.compare(password, candidate.password);
-      if (isSame) {
-        req.session.user = candidate;
-        req.session.isSignIn = true;
-        req.session.save(err => {
-          if (err) {
-            throw new Error(err);
-          } else {
-            res.redirect("/");
-          }
-        });
-      } else {
-        req.flash("loginErr", "Неверный пароль");
-        res.redirect("/auth/login");
-      }
-    } else {
-      req.flash("loginErr", "Пользователь не существует");
-      res.redirect("/auth/login");
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      req.flash("loginErr", errors.array()[0].msg);
+      return res.redirect("/auth/login");
     }
+
+    req.session.user = candidate;
+    req.session.isSignIn = true;
+    req.session.save(err => {
+      if (err) {
+        throw new Error(err);
+      } else {
+        res.redirect("/");
+      }
+    });
   } catch (e) {
     console.log(e);
   }
 });
 
-router.post("/signUp", async (req, res) => {
+router.post("/signUp", registerValidators, async (req, res) => {
   try {
-    const { email, name, password, confirm } = req.body;
-    const candidate = await User.findOne({ email });
-    if (candidate) {
-      req.flash("registerErr", "Пользователь уже существует");
-      res.redirect("/auth/login#signUp");
-    } else {
-      const hashPassword = await crypt.hash(password, 10);
-      const user = new User({ email, name, password: hashPassword, cart: [] });
-      await user.save();
-      res.redirect("/auth/login");
+    const { email, name, password } = req.body;
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      req.flash("registerErr", errors.array()[0].msg);
+      return res.status(422).redirect("/auth/login#signUp");
     }
+
+    const hashPassword = await crypt.hash(password, 10);
+    const user = new User({ email, name, password: hashPassword, cart: [] });
+    await user.save();
+    res.redirect("/auth/login");
   } catch (e) {
     console.log(e);
   }
